2bf1c5c129bc81ea3f7079bcd93334cf1eb215da
ponyjackal <pony1125@outlook.com>
2023-07-05 09:29:14 -0700

    feat: add post routes

--- routers/index.go
+++ routers/index.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ponyjackal/eventual-consistency-blog/controllers"
 )
 
 func RegisterRoutes(route *gin.Engine) {
@@ -13,4 +14,11 @@ func RegisterRoutes(route *gin.Engine) {
 	route.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"live": "ok"})
 	})
+
+		/* Controllers */
+		postController := controllers.NewPostController();
+
+	/* post routes */
+	route.GET("/posts", postController.GetPosts)
+	route.POST("/post", postController.SavePost)
 }
