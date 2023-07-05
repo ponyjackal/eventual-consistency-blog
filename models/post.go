b885083e720aa01f5569e9b349f84416d47853ad
ponyjackal <pony1125@outlook.com>
2023-07-05 08:14:51 -0700

    feat: add post model

---
+++ models/post.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/google/uuid"
+
+type Post struct {
+	ID		uuid.UUID 		`gorm:"type:uuid;column:id;primaryKey;default:gen_random_uuid()" json:"id"`
+
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Slug    string `json:"slug" gorm:"uniqueIndex"`
+}
